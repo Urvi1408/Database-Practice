dae5800399cb807233fc9aa2c39d7280cd43e345
Urvi1408 <winsome.urvi@gmail.com>
2026-07-20 20:35:49 +0530

    1581-customers who visited but didn't order

---
+++ Easy/customers_who_visited_but_didnt_order.sql
@@ -0,0 +1,6 @@
+select v.customer_id,count(t.transaction_id is null) as count_no_trans
+from Visits as v 
+left join Transactions as t 
+on v.visit_id=t.visit_id
+where t.transaction_id is null
+group by v.customer_id having count(*);
